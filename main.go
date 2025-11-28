64989ff60afa55be487933a19a8c73116475cb99
Daniel Rodrigues <danielrods2004@gmail.com>
2025-11-27 22:36:26 -0300

    Create dynamic student in DB

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-  "github.com/labstack/echo/v4"
-  "github.com/labstack/echo/v4/middleware"
-  "log/slog"
-  "net/http"
-  "errors"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"github.com/danrodsg/api-students/db"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -18,6 +21,10 @@ func main() {
 
   // Routes
   e.GET("/students", getStudent)
+  e.POST("/students", createStudent)
+  e.GET("/students/:id",getStudent)
+  e.PUT("/students/:id", updateStudent)
+  e.DELETE("/students/:id", deleteStudent)
 
   // Start server
   if err := e.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
@@ -26,6 +33,36 @@ func main() {
 }
 
 // Handler
-func getStudent(c echo.Context) error {
+
+func getStudents(c echo.Context) error {
   return c.String(http.StatusOK, "List of all students")
+}
+
+func createStudent(c echo.Context) error {
+	student := db.Student{}
+	if err := c.Bind(&student); err != nil {
+	    return err
+	}
+	db.AddStudent(student)
+  return c.String(http.StatusOK, "Create student")
+}
+
+func getStudent(c echo.Context) error {
+	id := c.Param("id")
+	getStud :=fmt.Sprintf("Get %s student", id)
+  return c.String(http.StatusOK, getStud)
+}
+
+func updateStudent(c echo.Context) error {
+	id := c.Param("id")
+	UpdateStud :=fmt.Sprintf("Update %s student", id)
+    return c.String(http.StatusOK, UpdateStud)
+	
+}
+
+func deleteStudent(c echo.Context) error {
+
+    id := c.Param("id")
+	deleteStud :=fmt.Sprintf("delete %s student", id)
+    return c.String(http.StatusOK, deleteStud)
 }
